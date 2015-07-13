e7b698af3af51b22124c731baabfab2e7f0f605b
Jim O'Halloran <jim@aligent.com.au>
2015-07-13 10:35:59 +0930

    Initial commit with cookbook skeleton

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'aligent-magento-dev'
+maintainer       '(c) 2015 Aligent Consulting'
+maintainer_email 'YOUR_EMAIL'
+license          'MIT'
+description      'Installs/Configures aligent-magento-dev'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
