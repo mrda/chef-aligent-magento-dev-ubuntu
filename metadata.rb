48fa5a42ad3354e829e2e915f0c02c2f53683e9c
Jim O'Halloran <jim@aligent.com.au>
2015-07-13 15:27:23 +0930

    Update database cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Custom recipes for an Aligent dev environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'database', '~> 2.3.0'
+depends 'database', '~> 4.0.6'
 depends 'nginx', '~> 2.7.6'
 depends 'php', '~> 1.3.12'
