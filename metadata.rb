20d8f2c28dfefaad05c2c70e2b36c272277af817
Jim O'Halloran <jim@aligent.com.au>
2015-07-13 15:12:53 +0930

    Create appropriate databases and users

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name             'aligent-magento-dev'
-maintainer       '(c) 2015 Aligent Consulting'
+maintainer       'Aligent Consulting'
 maintainer_email 'YOUR_EMAIL'
 license          'MIT'
-description      'Installs/Configures aligent-magento-dev'
+description      'Custom recipes for an Aligent dev environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'database', '~> 2.3.0'
+depends 'nginx', '~> 2.7.6'
+depends 'php', '~> 1.3.12'
---
+++ recipes/database.rb
@@ -0,0 +1,51 @@
+#
+## Cookbook Name:: aligent-magento-dev
+## Recipe:: database
+##
+## Copyright 2015, Aligent Consulting
+##
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the
+## "Software"), to deal in the Software without restriction, including
+## without limitation the rights to use, copy, modify, merge, publish,
+## distribute, sublicense, and/or sell copies of the Software, and to
+## permit persons to whom the Software is furnished to do so, subject to
+## the following conditions:
+##
+## The above copyright notice and this permission notice shall be
+## included in all copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+## MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+## NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+## LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+## OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+## WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+##
+#
+
+include_recipe "database::mysql"
+
+mysql_connection_info = {
+	:host => 'localhost',
+	:username => 'root',
+	:password => node['mysql']['server_root_password']
+}
+
+node['app']['mysql'].each do |db|
+    mysql_database db['database']  do
+        connection mysql_connection_info
+        action :create
+    end
+
+    mysql_database_user db['username'] do
+        connection mysql_connection_info
+        password db['password']
+        database_name db['database']
+        host db['acl']
+        privileges [:all]
+        action :grant
+    end
+
+end
