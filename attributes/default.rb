4d510b353ae2753f5dd19a6471b26e09bcd5d15f
Jim O'Halloran <jim@aligent.com.au>
2015-12-15 16:59:15 +1030

    Add Apache FastCGI based vhost declaration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['app']['xdebug']['enable'] = true
 default['app']['xdebug']['version'] = "stable"
+default['app']['xdebug']['remote_ip'] = "192.168.34.1"
+
+default['app']['magento_root'] = "/vagrant/public"
