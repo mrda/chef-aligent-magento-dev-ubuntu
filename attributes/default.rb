c6cc35cbefcbbb68933c33c776fd2ea67a39818d
Jim O'Halloran <jim@aligent.com.au>
2015-12-18 15:08:22 +1030

    Recipe to install Varnish, connect it to our existing backend, and configure it with a VCL appropriate for the Phoenix module.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,8 @@ default['app']['xdebug']['version'] = "stable"
 default['app']['xdebug']['remote_ip'] = "192.168.34.1"
 
 default['app']['magento_root'] = "/vagrant/public"
+
+default['varnish']['version'] = '3.0'
+default['varnish']['vcl_cookbook'] = 'aligent-magento-dev'
+default['varnish']['backend_host'] = 'localhost'
+default['varnish']['backend_port'] = 80
