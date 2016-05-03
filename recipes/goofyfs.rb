60331f0daeaca22119f0bded6478a6d41eeaacb5
Jim O'Halloran <jim@aligent.com.au>
2016-05-03 13:47:47 +0930

    Chef recipe to install goofyfs

---
+++ recipes/goofyfs.rb
@@ -0,0 +1,41 @@
+#
+## Cookbook Name:: aligent-magento-dev
+## Recipe:: goofyfs
+##
+## Copyright 2016, Aligent Consulting
+##
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the
+## "Software"), to deal in the Software without restriction, including
+## without limitation the rights to use, copy, modify, merge, publish,
+## distribute, sublicense, and/or sell copies of the Software, and to
+## permit persons to whom the Software is furnished to do so, subject to
+## the following conditions:
+##
+## The above copyright notice and this permission notice shall be
+## included in all copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+## MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+## NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+## LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+## OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+## WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+##
+#
+
+%w{fuse}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+remote_file '/usr/local/bin/goofyfs' do
+  source 'https://github.com/kahing/goofys/releases/download/v0.0.5/goofys'
+  owner 'root'
+  group 'root'
+  mode '0700'
+  action :create
+end
+
