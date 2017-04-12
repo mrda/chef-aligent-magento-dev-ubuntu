c337c7e8d428fa9c7a824e68c4ca4fb196c1ba7d
Jim O'Halloran <jim@aligent.com.au>
2017-04-12 11:32:49 +0930

    Don't hard code AWS region, pick up from environment vars

--- files/default/install-cloudwatch-logs-agent.sh
+++ files/default/install-cloudwatch-logs-agent.sh
@@ -2,12 +2,12 @@
 
 . /etc/profile.d/cf-stack-environment.sh
 
-pushd /tmp > /dev/null
+pushd /root > /dev/null
 
 process-template.sh "./cwlogs.cfg.template" "./cwlogs.cfg"
 
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
 chmod +x ./awslogs-agent-setup.py
-./awslogs-agent-setup.py -n -r us-east-1 -c ./cwlogs.cfg
+./awslogs-agent-setup.py -n -r $AWS_REGION -c ./cwlogs.cfg
 
 popd
