35726ac665e53c175ac4300e50571e258448206e
Tim Aslat <tim.aslat@aligent.com.au>
2017-04-27 12:36:03 +0930

    Fix typo in recipe to delete logfiles

--- recipes/mysql-server.rb
+++ recipes/mysql-server.rb
@@ -36,10 +36,10 @@ if node['app']['database_engine'] == 'mysql' || node['app']['database_engine'] =
     end
 
     %w{/var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile1 }.each do |logfile|
-
-    file logfile do
-      action :delete
-      only_if { File.exist? logfile }
+      file logfile do
+        action :delete
+        only_if { File.exist? logfile }
+      end
     end
 
     mysql_config 'default' do
