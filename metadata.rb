beb8bc427133b55cce17af8d069d45dea1d3b3b9
Michael Davies <michael@the-davies.net>
2017-09-22 15:26:09 +0930

    Adding ntp as a dep

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,12 @@ description      'Custom recipes for an Aligent dev environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+depends 'ntp', '~> 3.5.2'
 depends 'mysql2_chef_gem', '~> 1.0.2'
 depends 'database', '~> 4.0.6'
 depends 'nginx', '~> 2.7.6'
 depends 'php', '~> 1.8.0'
-depends 'mysql', '~> 6.0.30'
+depends 'mysql', '~> 8.0'
 depends 'redis2', '~> 0.5.1'
 depends 's3_file', '~> 2.7.0'
 depends 'varnish', '~> 3.3.0'
